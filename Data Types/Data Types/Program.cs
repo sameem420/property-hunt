e18f93faff54043acaa0229bc8267726d0f31524
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2023-01-04 18:05:48 +0400

    Data Types directory added

---
+++ Data Types/Data Types/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+int age = 24;
+float pi = 3.14F;
+string name = "Muhammad";
+char favChar = '^';
+bool isDeveloper = true;
+Console.WriteLine(name + " is " + age + " years old. " + pi + " is a floating point number." + "His favorite char is " + favChar + ". Is he a developer? " +  isDeveloper);
