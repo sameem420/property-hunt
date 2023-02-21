aea8ba4efc4c016e62ae6d93de744925144868b7
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-02-21 20:49:09 +0400

    operators covered

---
+++ Overview/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿int num1 = 6; int num2 = 15;
+
+int sumResult = num1 + num2;
+int subResult = num1 - num2;
+int multResult = num1 * num2;
+int divResult = num1 / num2;
+bool livingInUAE = true;
+bool livingInPAK = false;
+
+
+Console.WriteLine("Sum Result of two numbers is: " + sumResult);
+Console.WriteLine("Subtraction Result of two numbers is: " + subResult);
+Console.WriteLine("Multiplication Result of two numbers is: " + multResult);
+Console.WriteLine("Division Result of two numbers is: " + divResult);
+
+--num1;
+num1 += num2;
+++num1;
+num2 -= num1;
+num1++;
+num1 *= num1;
+++num2;
+num2 /= num1;
+
+
+Console.WriteLine("The Result is: " + num1);
+
+Console.WriteLine("AND OP: " + (livingInUAE && livingInPAK));
+Console.WriteLine("OR OP: " + (livingInUAE || livingInPAK));
+Console.WriteLine("NOT OP: " + !livingInPAK);
+
+
