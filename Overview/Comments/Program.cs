3b7251f2b1dbe88be9db93d609e79a6206283759
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-02-22 13:31:41 +0400

    ✅ comments covered

---
+++ Overview/Comments/Program.cs
@@ -0,0 +1,9 @@
+﻿// Printing a single number
+Console.WriteLine(21);
+
+// Printing the Sum of two numbers
+Console.WriteLine(34 + 61);
+
+/* Sum and multiplication of 
+two numbers */
+Console.WriteLine(6 * 5 + 12);
