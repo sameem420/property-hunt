4066bea9bb6e314d9288407345086113e19b01e4
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-03-04 01:55:23 +0400

    :white_check_mark: for loops covered!

---
+++ ProgramFlow/forLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int favNum = 21;
+int[] nums = new int[] {5,14,61,72,88};
+string quote = "A quick brown fox jumps over the lazy dog";
+var count = 0;
+
+for (int i = 1; i <= favNum; i++)
+{
+    Console.WriteLine("Iterating till : " + i);
+}
+
+foreach (var num in nums)
+{
+    Console.WriteLine("Printing random numbers : " + num);
+}
+
+foreach (var ch in quote)
+{
+    if(ch == 'o') {
+        count++;
+    }
+}
+ Console.WriteLine("Counted {0} o characters!", count );
