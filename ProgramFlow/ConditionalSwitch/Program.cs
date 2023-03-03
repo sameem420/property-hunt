a97218692b844abf028a233a2d3bba429d8231ba
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-03-04 01:37:34 +0400

    :white_check_mark: Switch control done!

---
+++ ProgramFlow/ConditionalSwitch/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Muhammad";
+
+switch (name)
+{
+    case "muhammad":
+        Console.WriteLine("Lower Case : does not match!");
+        break;
+    case "Muhammad":
+        Console.WriteLine("This matches exactly!");
+        break;
+    default:
+        Console.WriteLine("This does not match with any case!");
+        break;
+}
